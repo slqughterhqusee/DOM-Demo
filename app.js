02b726758366e4684fc232018a64c9e89d2a9108
icarante <slqughterhquse@gmail.com>
2024-02-15 09:29:19 -0800

    urgg

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+function getId(element){
+  return document.getElementById(element);
+}
+
+function getElements(selector){
+  document.querySelectorAll(selector);
+}
+
+console.log(getId('certificate'));
+console.log(getElements('a.button'));
+getElements('a.button').forEach(button => button.classList.add('whateverclass'));
+
 // Add Event listners when DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
 
     // check for buttons exist
-    if ( document.querySelectorAll('.button') !== null) {
-        let buttons = document.querySelectorAll('.button');
+    if ( getElements('.button') !== null) {
+        let buttons = getElements('.button');
         buttons.forEach(button => button.addEventListener('click', processForm))
     }
 
     // optionally update when fields update
     // tbd
+    getId('name').addEventListener('change', function (eventData) {
+      let nameOutput = document.getElementById('nameOutput');
+      nameOutput.innerText = eventData.target.value;
+    });
 
     // process form Data
     document.getElementById("myForm").addEventListener("submit", function (e) {
@@ -26,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Log readiness to console
     console.log("Ready");
 });
+
 
 function processForm(form){
 
