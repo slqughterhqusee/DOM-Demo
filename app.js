f3e0b43a38e9b7b5e6c6eb13475451022adca456
icarante <slqughterhquse@gmail.com>
2024-02-16 10:37:25 -0800

    THE BACKGROUND POPS UP!!

--- app.js
+++ app.js
@@ -6,24 +6,30 @@ function getElements(selector){
   document.querySelectorAll(selector);
 }
 
-console.log(getId('certificate'));
-console.log(getElements('a.button'));
-getElements('a.button').forEach(button => button.classList.add('whateverclass'));
+// console.log(getId('certificate'));
+// console.log(getElements('a.button'));
+// getElements('a.button').forEach(button => button.classList.add('whateverclass'));
 
 // Add Event listners when DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
 
     // check for buttons exist
-    if ( getElements('.button') !== null) {
-        let buttons = getElements('.button');
-        buttons.forEach(button => button.addEventListener('click', processForm))
-    }
+    // if ( getElements('.button') !== null) {
+    //     let buttons = getElements('.button');
+    //     buttons.forEach(button => button.addEventListener('click', processForm))
+    // }
 
     // optionally update when fields update
     // tbd
     getId('name').addEventListener('change', function (eventData) {
       let nameOutput = document.getElementById('nameOutput');
       nameOutput.innerText = eventData.target.value;
+    });
+
+    getId('type').addEventListener('change', function (eventData) {
+      // let nameOutput = document.getElementById('nameOutput');
+      // nameOutput.innerText = eventData.target.value;
+      changeCert(eventData.target.value);
     });
 
     // process form Data
@@ -39,6 +45,22 @@ document.addEventListener("DOMContentLoaded", function () {
           console.log(pair[0] + ": " + pair[1]);
         }
       });
+
+      function changeCert(value){
+        let certificate = document.getElementById('certificate');
+        certificate.classList = ""; // clear classes on each function call
+        switch(value){
+            case 'A':
+                certificate.classList.add('cactuar');
+            break;
+            case 'B':
+                certificate.classList.add('axel');
+            break;
+            case 'C':
+                certificate.classList.add('deuce');
+            break;
+        }
+    }
     // Log readiness to console
     console.log("Ready");
 });
