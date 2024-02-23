addcdbe4e5571d2e3d57ad331f7662040e1ae23a
icarante <slqughterhquse@gmail.com>
2024-02-23 10:43:22 -0800

    urmmmmmmm

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function getId(element){
+function getId(element) {
   return document.getElementById(element);
 }
 
-function getElements(selector){
+function getElements(selector) {
   document.querySelectorAll(selector);
 }
 
@@ -13,59 +13,67 @@ function getElements(selector){
 // Add Event listners when DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
 
-    // check for buttons exist
-    // if ( getElements('.button') !== null) {
-    //     let buttons = getElements('.button');
-    //     buttons.forEach(button => button.addEventListener('click', processForm))
+  // check for buttons exist
+  // if (getElements('.button') !== null) {
+  //   let buttons = getElements('.button');
+  //   buttons.forEach(button => button.addEventListener('click', processForm))
+  // }
+
+  // optionally update when fields update
+  // tbd
+  getId('name').addEventListener('change', function (eventData) {
+    let nameOutput = document.getElementById('nameOutput');
+    nameOutput.innerText = eventData.target.value;
+  });
+
+  getId('type').addEventListener('change', function (eventData) {
+    let nameOutput = document.getElementById('nameOutput');
+    // let info = getId('info');
+    // nameOutput.innerText = eventData.target.value;
+    // info.innerText = eventData.target.value;
+    changeCert(eventData.target.value);
+  });
+
+  // process form Data
+  document.getElementById("myForm").addEventListener("submit", function (eventData) {
+    eventData.preventDefault();
+    console.log(eventData.target);
+    var formData = new FormData(eventData.target);
+    formData = Object.fromEntries;
+    // output as an object
+    console.log(Object.fromEntries(formData));
+
+    // ...or iterate through the name-value pairs
+    // for (var pair of formData.entries()) {
+    //   console.log(pair[0] + ": " + pair[1]);
     // }
+  });
 
-    // optionally update when fields update
-    // tbd
-    getId('name').addEventListener('change', function (eventData) {
-      let nameOutput = document.getElementById('nameOutput');
-      nameOutput.innerText = eventData.target.value;
-    });
+  // Log readiness to console
+  console.log("Ready");
 
-    getId('type').addEventListener('change', function (eventData) {
-      // let nameOutput = document.getElementById('nameOutput');
-      // nameOutput.innerText = eventData.target.value;
-      changeCert(eventData.target.value);
-    });
-
-    // process form Data
-    document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault();
-      
-        var formData = new FormData(form);
-        // output as an object
-        console.log(Object.fromEntries(formData));
-      
-        // ...or iterate through the name-value pairs
-        for (var pair of formData.entries()) {
-          console.log(pair[0] + ": " + pair[1]);
-        }
-      });
-
-      function changeCert(value){
-        let certificate = document.getElementById('certificate');
-        certificate.classList = ""; // clear classes on each function call
-        switch(value){
-            case 'A':
-                certificate.classList.add('cactuar');
-            break;
-            case 'B':
-                certificate.classList.add('axel');
-            break;
-            case 'C':
-                certificate.classList.add('deuce');
-            break;
-        }
+  function changeCert(value) {
+    let certificate = document.getElementById('certificate');
+    certificate.classList = ""; // clear classes on each function call
+    switch (value) {
+      case 'A':
+        certificate.classList.add('cactuar');
+        break;
+      case 'B':
+        certificate.classList.add('axel');
+        break;
+      case 'C':
+        certificate.classList.add('deuce');
+        break;
     }
-    // Log readiness to console
-    console.log("Ready");
+  }
+
+  let theDate = new Date().toLocaleDateString('en-us', {weekday:"long", year:"numeric", month:"numeric", day:"numeric"})
+  let dateBox = getId('theDate');
+  dateBox.innerText= theDate;
 });
 
 
-function processForm(form){
+function processForm(form) {
 
 }
